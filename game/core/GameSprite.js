22cdfe1b14513e70b87f57a4a92252f42ae0f96c
Rundll86 <2028673870@qq.com>
2023-11-19 16:20:00 +0800

    Update

--- game/core/GameSprite.js
+++ game/core/GameSprite.js
@@ -1,13 +1,23 @@
+const DataGenerator = require("./DataGenerator.js");
 const GameObject = require("./GameObject.js");
 const ResourceLoader = require("./ResourceLoader.js");
 class GameSprite extends GameObject {
     DisplayName = "GameSprite";
     ID = 0;
     Describe = "A common game sprite.";
-    ActionPath = {}
+    ActionPath = DataGenerator.ActionPath(this.DisplayName);
     constructor() {
         super();
         ResourceLoader.Init();
+    };
+    LoadResource() {
+        for (let i in this.ActionPath) {
+            /**
+             * @type {ResourceLoader}
+             */
+            let Loader = window.ResourceExcel;
+            Loader.LoadBitmapToExcel(this.ActionPath[i], i);
+        };
     };
 };
 module.exports = GameSprite;
