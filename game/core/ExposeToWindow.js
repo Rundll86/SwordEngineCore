a9beb14577436d0401516c7d97af7401eec8636c
Rundll86 <108387605+Rundll86@users.noreply.github.com>
2023-11-25 09:01:26 +0800

    Delete game directory

--- game/core/ExposeToWindow.js
+++
@@ -1,5 +0,0 @@
-module.exports = function ExposeToWindow(Data, Name = null) {
-    if (!Name) { return; };
-    window[Name] = Data;
-    return Data;
-};
--- game/core/GameObject.js
+++
@@ -1,6 +0,0 @@
-class GameObject {
-    DisplayName = "GameObject";
-    ID = 0;
-    Describe = "A common game object.";
-};
-module.exports = GameObject;
